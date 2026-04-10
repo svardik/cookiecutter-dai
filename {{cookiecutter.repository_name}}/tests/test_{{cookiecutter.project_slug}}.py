8fe88c3f9220d86240f63e7c4bb138c5e221492e
svardik <67322869+svardik@users.noreply.github.com>
2026-04-10 15:53:04 -0400

    minor changes to make setup easier

--- {{cookiecutter.repository_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.repository_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Tests for `{{ cookiecutter.project_slug }}` package."""
-
 import pytest
+
 
 @pytest.fixture
 def setup_teardown():
     """Set up and tear down test fixtures, if any."""
     yield
 
-@pytest.mark.usefixtures("setup_teardown")
+
+@pytest.mark.usefixtures('setup_teardown')
 def test_000_something():
     """Test something."""
     assert True
